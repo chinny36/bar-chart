e04424645c29e8336f068b2f882e096caa694d43
chinny36 <brownj95@gmail.com>
2021-03-17 10:18:35 -0400

    Began refactoring the html into a JS function

--- bar-chart.js
+++ bar-chart.js
@@ -1,14 +1,18 @@
 drawBarChart = (data, options, element) => {
-  let newChart = document.createElement(element);
+  const newChart = document.createElement(element);
+  newChart.classList.add('chart');
   let chartStyle = {
     width: options.width,
     height: options.height,
     display: 'block'
   };
+
+
   Object.assign(newChart, chartStyle);
 
 
   document.body.insertAdjacentElement('afterbegin', newChart);
+
 };
 
 drawTitle = () => {
@@ -28,7 +32,7 @@ drawNumbers = () => {
 const data = [1,2,3,4,5];
 const options = {
   width: 600,
-  height: 400,
+  height: 500,
   barColor: 'green',
   labelColor: 'white',
   barSpacing: 20,
@@ -36,4 +40,30 @@ const options = {
   titleColor: 'white',
   yAxis: [0,25,50,75,100]
 };
-const element = 'div.chart';
+const element = 'div';
+
+{/* <div class = 'chart'>
+<h1 class = 'title'>Bar Chart</h1>
+<ul class = 'numbers'>
+  <li><span>100</span></li>
+  <li><span>75</span></li>
+  <li><span>50</span></li>
+  <li><span>25</span></li>
+  <li><span>0</span></li>
+</ul>
+<ul class = 'bars'>
+  <li><div class = 'bar' data-percent = 60></div><span>foccfdso</span></li>
+  <li><div class = 'bar' data-percent = 40></div><span>Entry 2</span></li>
+  <li><div class = 'bar' data-percent = 80></div><span>Entry 3</span></li>
+  <li><div class = 'bar' data-percent = 100></div><span>Entry 5</span></li>
+  <li><div class = 'bar' data-percent = 100></div><span>Entry 5</span></li>
+</ul>
+</div>
+
+<script type = 'text/javascript'>
+$(barAnimation = () => {
+  $('.bar').each(function(){
+    $(this).animate({'height': $(this).data('percent') + '%'}, 1500);
+  });
+});
+</script> */}
